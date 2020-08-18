ca8eb60a3a6efd685436b3945d648c025f9d1e93
edmundo papi <edmundopierre@yahoo.com>
2020-08-18 14:00:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -44,4 +44,10 @@ def values_for_insert
   end
   values.join(", ")
 end
+def save
+  DB[:conn].execute ("INSERT INTO #{table_name_for_insert}(
+  #{col_names_for_insert}) values(?)",
+  [values_for_insert])
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+end
 end
