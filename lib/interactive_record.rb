8eb27211ff78a596227475cd8e6e85dbf9d579ac
edmundo papi <edmundopierre@yahoo.com>
2020-08-18 13:37:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,7 +33,6 @@ attr_accessor :id , :name , :grade
 def table_name_for_insert
   self.class.table_name
 end
-binding.pry
 def col_names_for_insert
   self.class.column_names.delete_if { |col| col==id  }.join(", ")
 end
